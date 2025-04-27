6355f2cf6e91c3244a2a5a5156a0c03a90229fc0
Mondestin Myssie <sydneymondestin@gmail.com>
2025-04-27 14:04:51 +0200

    uber importation updated

--- src/features/reports/components/csv-data-table.tsx
+++ src/features/reports/components/csv-data-table.tsx
@@ -11,7 +11,12 @@ interface DriverData {
   'Prénom du chauffeur': string
   'Nom du chauffeur': string
   'Revenus totaux': string,
-  'Versements': string
+  'Versements': string,
+  'Remboursements et notes de frais': string,
+  'Revenus totaux:Bonus': string,
+  'Revenus totaux:Autres revenus:Ajustement des frais de service liés aux courses partagées': string,
+  'Revenus totaux:Autres revenus:Retour d\'un objet oublié': string,
+  'Montant versé à des tiers': string
 }
 
 export function CsvDataTable({ data, headers }: { data: DriverData[], headers: string[] }) {
--- src/features/reports/components/import-form.tsx
+++ src/features/reports/components/import-form.tsx
@@ -38,7 +38,11 @@ interface DriverData {
   'Nom du chauffeur': string
   'Revenus totaux': string,
   'Versements': string,
-  'Remboursements et notes de frais': string
+  'Remboursements et notes de frais': string,
+  'Revenus totaux:Bonus': string,
+  'Revenus totaux:Autres revenus:Ajustement des frais de service liés aux courses partagées': string,
+  'Revenus totaux:Autres revenus:Retour d\'un objet oublié': string,
+  'Montant versé à des tiers': string
 }
 
 interface BoltDriverData {
